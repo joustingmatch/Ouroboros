6ed0397d24b3a60cfebc689242205899e3f50153
joustingmatch <vulnerablefemboy3@gmail.com>
2026-07-28 22:56:01 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -85,6 +85,7 @@ local games = {
     [5003223] = 'slapacumslut',
     [14685986] = 'capybarasvsplants.lua',
     [492855504] = 'crawfishing.lua',
+    [522637919] = 'miniwar.lua',
 }   
 
 local file = games[game.CreatorId]
