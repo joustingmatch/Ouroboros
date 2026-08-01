e2884ba91e42ec1621fc9b818093373ee9db15b1
joustingmatch <vulnerablefemboy3@gmail.com>
2026-08-01 09:16:54 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -99,6 +99,7 @@ local games = {
     [389736281] = 'saveanimals.lua',
     [35328876] = 'wtfisthisgamebrolmfao.lua',
     [645519191] = 'mergeamob.lua',
+    [622013004] = 'cleankeycap.lua',
 }   
 
 local file = games[game.CreatorId]
