e8d6f67e74701e5a9dc834da7d20eb5b4edbed04
joustingmatch <vulnerablefemboy3@gmail.com>
2026-08-19 17:40:54 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -229,6 +229,7 @@ local games = {
     [5019929] = 'penthouse.lua',
     [976904614] = 'animegirlpaint.lua',
     [33017480] = 'animedice.lua',
+    [697359830] = 'reeled.lua',
 }   
 
 local file = games[game.CreatorId]
