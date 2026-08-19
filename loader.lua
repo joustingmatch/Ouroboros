0e6e6a79119479989f2969f7ed0b44200bc7f382
joustingmatch <vulnerablefemboy3@gmail.com>
2026-08-19 14:43:31 -0400

    Add penthouse.lua to games list

--- loader.lua
+++ loader.lua
@@ -226,6 +226,7 @@ local games = {
     [5096106] = 'surviveanimearena.lua',
     [684188376] = 'rebirthfrenzy.lua',
     [177982364] = 'hoteltycoon.lua',
+    [5019929] = 'penthouse.lua',
 }   
 
 local file = games[game.CreatorId]
