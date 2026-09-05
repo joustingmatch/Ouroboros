e3e08d4a3132e59b35f869ad784eb78771b68ec3
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-04 21:38:39 -0400

    Add new script 'slopslopslopslopslop.luau' to loader

--- loader.lua
+++ loader.lua
@@ -16,6 +16,7 @@ local games = {
     [561990553]  = 'survive-zombie-arena.lua',
     [35906875]   = 'anime-story-2.lua',
     [457402993] = 'longarm.luau',
+    [67078391] = 'slopslopslopslopslop.luau',
     [33910482]   = 'anime-world-fighters.lua',
     [895955624]  = 'anime-rng.lua',
     [823798692] = 'haqiqi.luau',
