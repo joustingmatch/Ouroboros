83d55a76e91ebca29e2146560a60e67f644d3d85
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-06 00:40:05 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -25,6 +25,7 @@ local games = {
     [895955624]  = 'anime-rng.lua',
     [823798692] = 'haqiqi.luau',
     [33910482]   = 'anime-astral-simulator.lua',
+    [574558180] = 'vibecodedslop.luau',
     [1057773493] = 'hatchandfry.luau',
     [1006239440] = 'anime-battle-rng.lua',
     [572660282]  = 'anime-ultraon-simulator.lua',
