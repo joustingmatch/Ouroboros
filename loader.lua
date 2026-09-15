adcd61698d80678bdd4d65c5f044bebc6d703a3f
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-15 04:46:00 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -16,6 +16,7 @@ local games = {
     [446405201]  = 'merge-a-nuke.lua',
     [290340269] = 'dinogame.luau',
     [1040745973] = 'laststop.luau',
+    [705496325] = 'aislop.luau',
     [828376264] = 'lootforge.luau',
     [7020486356] = 'endlesstower.luau',
     [604593435] = 'cummingdrivethru.luau',
