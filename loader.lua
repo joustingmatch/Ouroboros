009d9f10389b322c83c8e3c1bfdb5bb904e41d4f
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-17 07:41:22 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,6 +12,7 @@ local games = {
     [973045631]  = 'anime-card-farm.lua',
     [104489519]  = 'defend-ur-base-with-anime.lua',
     [32744161] = 'projectaurarng.luau',
+    [882466945] = 'rollforverity.luau',
     [899260384] = 'mergescp.luau',
     [8204913] = 'utd.luau',
     [446405201]  = 'merge-a-nuke.lua',
