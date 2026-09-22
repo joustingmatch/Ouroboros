235ad163800608c89c4fe137a07b82c51488c666
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-22 10:29:26 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,6 +11,7 @@ local games = {
     [759293173]  = 'reign-piece.lua',
     [604931455] = 'ewjob.luau',
     [973045631]  = 'anime-card-farm.lua',
+    [424082979] = 'fuckingbitchegg.luau',
     [104489519]  = 'defend-ur-base-with-anime.lua',
     [32744161] = 'projectaurarng.luau',
     [948557217] = 'animerngx.luau',
