80a8a0a9928a0948366346b4a522cb7b1accfcca
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-22 08:33:12 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -14,6 +14,7 @@ local games = {
     [104489519]  = 'defend-ur-base-with-anime.lua',
     [32744161] = 'projectaurarng.luau',
     [948557217] = 'animerngx.luau',
+    [202162692] = 'cumnigga.luau',
     [88638261] = 'robverity.luau',
     [354756623] = 'shitass.luau',
     [656960909] = 'veritylava.luau',
