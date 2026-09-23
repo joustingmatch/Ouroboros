d325e10fe6976a2eccd4c2b41acf36c3b66bcc99
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-23 09:50:48 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,6 +7,7 @@ local BASE = 'https://raw.githubusercontent.com/joustingmatch/Ouroboros/main/gam
 local games = {
     [9190691]    = 'anime-squadron.lua',
     [896806231]  = 'axe-rng.lua',
+    [479094425] = 'suck.luau',
     [374427578] = 'hitagolfball.luau',
     [759293173]  = 'reign-piece.lua',
     [604931455] = 'ewjob.luau',
