c527e22bb4007072c0c57755f4c4a43e390e6943
joustingmatch <vulnerablefemboy3@gmail.com>
2026-09-24 23:10:25 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -18,6 +18,7 @@ local games = {
     [207366578] = 'skiphoe.luau',
     [973045631]  = 'anime-card-farm.lua',
     [846226343] = 'surfverity.luau',
+    [32680696] = 'cumcum.luau',
     [424082979] = 'fuckingbitchegg.luau',
     [104489519]  = 'defend-ur-base-with-anime.lua',
     [473699786] = 'cupverity.luau',
